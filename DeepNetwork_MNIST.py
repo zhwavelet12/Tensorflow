238416b2d17295bd72e98cc59507b553af067a9e
zhwavelet12 <zhwavelet@163.com>
2018-05-01 12:31:41 +0800

    add tensor flow entance exercise https://www.bilibili.com/video/av14452222/?p=39 excercise

---
+++ DeepNetwork_MNIST.py
@@ -0,0 +1,165 @@
+
+# coding: utf-8
+
+# In[36]:
+
+
+import tensorflow as tf
+#from tensorflow.examples.tourials.minist import input_data
+import input_data
+
+logPath=".\\tb_logs"
+
+mnist = input_data.read_data_sets("H:\\zhou\\stanford_TF\\data\\MNIST", one_hot=True)
+
+sess=tf.InteractiveSession()    
+#sess=tf.InteractiveSession()
+with tf.name_scope("MNIST_Input"):
+    x = tf.placeholder(tf.float32, [None,784],name="x")
+    y_ = tf.placeholder(tf.float32, [None,10],name="y_")
+
+def variable_summaries(var):
+    with tf.name_scope("sumaries"):
+        mean=tf.reduce_mean(var)
+        tf.summary.scalar("mean",mean)
+        with tf.name_scope("stddev"):
+            stddev=tf.sqrt(tf.reduce_mean(tf.square(var-mean)))
+        tf.summary.scalar("stddev",stddev)
+        tf.summary.scalar("max",tf.reduce_max(var))
+        tf.summary.scalar("min",tf.reduce_min(var))
+        tf.summary.histogram("histogram",var)
+        
+#定义一个函数，用于初始化所有的权值 W
+def weight_variable(shape,name=None):
+    initial = tf.truncated_normal(shape, stddev=0.1,name=name)
+    return tf.Variable(initial)
+
+#定义一个函数，用于初始化所有的偏置项 b
+def bias_variable(shape,name=None):
+    initial = tf.constant(0.1, shape=shape,name=name)
+    return tf.Variable(initial)
+  
+#定义一个函数，用于构建卷积层
+def conv2d(x, W,name=None):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME',name=name)
+
+#定义一个函数，用于构建池化层
+def max_pool(x,name=None):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='SAME',name=name)
+
+#构建网络
+with tf.name_scope("Input_Reshape"):
+    x_image = tf.reshape(x, [-1,28,28,1],name="x_Image")         #转换输入数据shape,以便于用于网络中
+
+with tf.name_scope("Conv1"):
+    with tf.name_scope("weights"):
+        W_conv1 = weight_variable([5, 5, 1, 32],name="weight") 
+        variable_summaries(W_conv1)
+    with tf.name_scope("biases"):
+        b_conv1 = bias_variable([32],name="bias") 
+        variable_summaries(b_conv1)
+    conv1_wx_b = conv2d(x_image, W_conv1, name="conv2d") + b_conv1
+    tf.summary.histogram("conv1_wx_b",conv1_wx_b)
+    
+    h_conv1 = tf.nn.relu(conv1_wx_b,name="relu")     #第一个卷积层
+    tf.summary.histogram("h_conv1",h_conv1)
+    
+    h_pool1 = max_pool(h_conv1, name="pool")                                  #第一个池化层
+
+with tf.name_scope("Conv2"):
+    with tf.name_scope("weights"):
+        W_conv2 = weight_variable([5, 5, 32, 64],name="weight")
+        variable_summaries(W_conv2)
+    with tf.name_scope("biases"):
+        b_conv2 = bias_variable([64],name="bias")
+        variable_summaries(b_conv2)
+    conv2_wx_b = conv2d(h_pool1, W_conv2,name="conv2d") + b_conv2
+    tf.summary.histogram("conv2_wx_b",conv2_wx_b)
+    
+    h_conv2 = tf.nn.relu(conv2_wx_b,name="relu")      #第二个卷积层  
+    tf.summary.histogram("h_conv2",h_conv2)
+        
+    h_pool2 = max_pool(h_conv2,name="pool")                                   #第二个池化层
+
+with tf.name_scope("FC"):
+    W_fc1 = weight_variable([7 * 7 * 64, 1024],name="weight")
+    b_fc1 = bias_variable([1024],name="bias")
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])    #reshape成向量 x2 why
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1,name="relu")    #第一个全连接层
+
+keep_prob = tf.placeholder(tf.float32,name="keep_prob") 
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)                  #dropout层
+variable_summaries(h_fc1_drop)
+
+
+with tf.name_scope("Readout"):
+    W_fc2 = weight_variable([1024, 10],name="weight")
+    b_fc2 = bias_variable([10],name="bias")
+    variable_summaries(W_fc2)
+    variable_summaries(b_fc2)
+    
+    #define model
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    #y_predict=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)   #softmax层
+    variable_summaries(y_conv)
+
+learning_rate = tf.placeholder(tf.float32, shape=[])
+tf.summary.scalar("learning_rate", learning_rate)
+
+#cross_entropy = -tf.reduce_sum(y_actual*tf.log(y_predict))     #交叉熵
+with tf.name_scope("cross_entropy"):
+    #cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv,labels=y_))     #交叉熵
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv)
+    variable_summaries(cross_entropy)
+    
+with tf.name_scope("loss_optimizer"):
+    #train_step = tf.train.GradientDescentOptimizer(1e-4).minimize(cross_entropy)    #梯度下降法
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+    
+with tf.name_scope("accuracy"):
+    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))    
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))                 #精确度计算
+
+#tf.summary.scalar("cross_entropy_sc1",cross_entropy)
+#tf.summary.scalar("training_accuracy",accuracy)
+
+merged = tf.summary.merge_all()
+tbWriter = tf.summary.FileWriter(logPath, sess.graph)
+
+#sess.run(tf.initialize_all_variables())
+#sess.run(tf.global_variables_initializer())
+tf.global_variables_initializer().run()
+
+import time
+
+#num_steps = 3000
+#display_every = 100
+num_steps = 30
+display_every = 10
+
+start_time=time.time()
+end_time = time.time()
+
+#train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    
+for i in range(num_steps):
+    batch = mnist.train.next_batch(50)
+    #sess.run(train_step,feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    #_,summary = sess.run([train_step,summarize_all], feed_dict={x: batch[0], y_: batch[1], learning_rate:0.02, keep_prob: 0.5})
+    #summary, _=sess.run([merged,train_step], feed_dict={x: batch[0], y_: batch[1], learning_rate:0.02, keep_prob: 0.5})
+    summary, _= sess.run([merged, train_step], feed_dict={x:batch[0], y_: batch[1], learning_rate:0.02, keep_prob: 0.5})
+    
+    if i%display_every == 0:                  #训练100次，验证一次
+        train_acc = accuracy.eval(feed_dict={x:batch[0], y_:batch[1], keep_prob: 1.0})
+        print("step {0}, training accuracy {1:.2f}".format(i,train_acc))
+        tbWriter.add_summary(summary, i)
+        
+end_time = time.time()
+print("Total training time for {0} bathes :{1:.2f} seconds".format(i+1,end_time-start_time))
+
+# cannot run the eval funciton it will cause the mem read too much.
+#test_acc=accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+#print("test accuracy {0:.2f}".format(test_acc))
+
+sess.close()
+
---
+++ Regression.py
@@ -0,0 +1,156 @@
+import tensorflow as tf
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation #import animation support
+
+num_house = 160
+np.random.seed(42)
+house_size = np.random.randint(low=1000,high=3500,size=num_house)
+
+np.random.seed(42)
+house_price = house_size*100.0+np.random.randint(low=20000,high=70000,size=num_house)
+
+# Plot generated house and size  
+plt.plot(house_size, house_price, "bx")  # bx = blue x 
+plt.ylabel("Price") 
+plt.xlabel("Size") 
+plt.show()
+
+# you need to normalize values to prevent under/overflows. 
+def normalize(array):
+    return (array - array.mean()) / array.std()
+
+# define number of training samples, 0.7 = 70%.  
+
+# We can take the first 70% since the values are randomized 
+
+num_train_samples = math.floor(num_house * 0.7)  
+
+
+
+# define training data 
+
+train_house_size = np.asarray(house_size[:num_train_samples]) 
+
+train_price = np.asanyarray(house_price[:num_train_samples:])  
+
+train_house_size_norm = normalize(train_house_size) 
+
+train_price_norm = normalize(train_price)  
+
+train_house_size_std = train_house_size.std()
+train_house_size_mean = train_house_size.mean()
+train_price_std = train_price.std()
+train_price_mean = train_price.mean()
+
+# define test data 
+
+test_house_size = np.array(house_size[num_train_samples:]) 
+
+test_house_price = np.array(house_price[num_train_samples:])  
+
+test_house_size_norm = normalize(test_house_size) 
+
+test_house_price_norm = normalize(test_house_price)
+
+#  Set up the TensorFlow placeholders that get updated as we descend down the gradient
+#   Constant: constant value
+#   Variable: values adjusted in graph
+#   PlaceHolder: used to pass data into graph.
+#
+#Place holder example:
+#x = tf.placeholder(tf.float32, shape=(1024, 1024))  
+#y = tf.matmul(x, x)  
+#  
+#with tf.Session() as sess:  
+#  print(sess.run(y))  # ERROR: x is not put value with feed_dict
+#  
+#  rand_array = np.random.rand(1024, 1024)  
+#  print(sess.run(y, feed_dict={x: rand_array}))  # Will succeed. 
+#
+#
+tf_house_size = tf.placeholder("float", name="house_size")
+tf_price = tf.placeholder("float", name="price")
+
+print(tf_house_size)
+print(tf_price)
+
+# Define the variables holding the size_factor and price we set during training.  
+# We initialize them to some random values based on the normal distribution.
+tf_size_factor = tf.Variable(initial_value=np.random.randn(), name="size_factor")
+tf_price_offset = tf.Variable(initial_value=np.random.randn(), name="price_offset")
+print(tf_size_factor)
+print(tf_price_offset)
+
+# 2. Define the operations for the predicting values - predicted price = (size_factor * house_size ) + price_offset
+#  Notice, the use of the tensorflow add and multiply functions.  These add the operations to the computation graph,
+#  AND the tensorflow methods understand how to deal with Tensors.  Therefore do not try to use numpy or other library 
+#  methods.
+tf_price_pred = tf.add(tf.multiply(tf_size_factor, tf_house_size), tf_price_offset)
+
+# 3. Define the Loss Function (how much error) - Mean squared error
+tf_cost = tf.reduce_sum(tf.pow(tf_price_pred-tf_price, 2))/(2*num_train_samples)
+
+# Optimizer learning rate.  The size of the steps down the gradient
+learning_rate = 0.1
+
+# 4. define a Gradient descent optimizer that will minimize the loss defined in the operation "cost".
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(tf_cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# keep iterating the training data
+display_every=1
+num_training_iter = 1
+sess = tf.Session()
+sess.run(init)
+fit_plot_idx=1
+fit_size_factor = np.empty(math.floor(num_training_iter + 1 ))
+fit_price_offsets = np.empty(math.floor(num_training_iter + 1))
+
+for iteration in range(num_training_iter):
+    # Fit all training data
+    for (x, y) in zip(train_house_size_norm, train_price_norm):
+        sess.run(optimizer, feed_dict={tf_house_size:x, tf_price:y})
+        # Display current status
+        if (iteration + 1) % display_every == 0:
+            c = sess.run(tf_cost, feed_dict={tf_house_size: train_house_size_norm, tf_price:train_price_norm})
+            print("iteration #:", '%04d' % (iteration + 1), "cost=", "{:.9f}".format(c), \
+                "size_factor=", sess.run(tf_size_factor), "price_offset=", sess.run(tf_price_offset))
+            # Save the fit size_factor and price_offset to allow animation of learning process
+    fit_size_factor[fit_plot_idx] = sess.run(tf_size_factor)
+    fit_price_offsets[fit_plot_idx] = sess.run(tf_price_offset)
+    fit_plot_idx = fit_plot_idx + 1
+
+print("Optimization Finished!")
+training_cost = sess.run(tf_cost, feed_dict={tf_house_size: train_house_size_norm, tf_price: train_price_norm})
+print("Trained cost=", training_cost, "size_factor=", sess.run(tf_size_factor), "price_offset=", sess.run(tf_price_offset), '\n')
+
+# 
+# Plot another graph that animation of how Gradient Descent sequentually adjusted size_factor and price_offset to 
+# find the values that returned the "best" fit line.
+fig, ax = plt.subplots()
+line, = ax.plot(house_size, house_price)
+
+plt.rcParams["figure.figsize"] = (10,8)
+plt.title("Gradient Descent Fitting Regression Line")
+plt.ylabel("Price")
+plt.xlabel("Size (sq.ft)")
+plt.plot(train_house_size, train_price, 'go', label='Training data')
+plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
+
+def animate(i):
+    line.set_xdata(train_house_size_norm * train_house_size_std + train_house_size_mean)  # update the data
+    line.set_ydata((fit_size_factor[i] * train_house_size_norm + fit_price_offsets[i]) * train_price_std + train_price_mean)  # update the data
+    return line,
+ 
+# Init only required for blitting to give a clean slate.
+def initAnim():
+    line.set_ydata(np.zeros(shape=house_price.shape[0])) # set y's to 0
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, frames=np.arange(0, fit_plot_idx), init_func=initAnim,
+                                 interval=1000, blit=True)
+plt.show() 
